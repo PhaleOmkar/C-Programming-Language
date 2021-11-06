42c0d0c1d43180fb4cc4b7bcf900ca37db4494aa
Omkar Phale <44401060+PhaleOmkar@users.noreply.github.com>
2021-11-06 18:52:38 +0530

    Multiplication Of Two Integers

---
+++ MultiplicationOfTwoNumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int first_number = 10;
+	int second_number = 20;
+	int resultant_number = 0;
+
+	resultant_number = first_number * second_number;
+
+	printf("%d * %d = %d\n", first_number, second_number, resultant_number);
+
+	return(0);
+}
+
