39add228f1a2229251c2eaa8d814aea276567f95
Omkar Phale <44401060+PhaleOmkar@users.noreply.github.com>
2021-11-06 18:52:55 +0530

    Division Of Two Integers

---
+++ DivisionOfTwoNumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int first_number = 10;
+	int second_number = 20;
+	int resultant_number = 0;
+
+	resultant_number = second_number / first_number;
+
+	printf("%d / %d = %d\n", second_number, first_number, resultant_number);
+
+	return(0);
+}
+
